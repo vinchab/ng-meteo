1266ac2155591edd0f59ad786b31a2c37f6a0d20
Vincent Chablaix <vincent@imac-de-vincent-1.home>
2020-07-03 10:10:36 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Motion, Geolocation } from '@capacitor/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-cap';
+export class AppComponent implements OnInit {
+
+  WeatherData:any;
+  private _data$: BehaviorSubject<any> = new BehaviorSubject(null)
+  public data$: Observable<any> = this._data$.asObservable()
+
+  title = 'CapacitorDemo';
+  orientationListenerActive = false;
+  alpha
+  beta
+  gamma
+
+  latitude: number = 46.207175299999996
+  longitude: number = 6.0998024
+  appId: string = '7cb6e2f8304ca90d850a207ca64163d2'
+
+  constructor(
+    private _http: HttpClient
+  ) { 
+    this.getLocation()
+  }
+
+  ngOnInit() {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
+    this.getWeatherData();
+    console.log(this.WeatherData);
+  }
+
+  getWeatherData(){
+    /* https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appId}&units=metric */
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&appid=${this.appId}`)
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+  }
+
+  setWeatherData(data){
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
+    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+  }
+
+  async getLocation() {
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+  }
+
+
+
+  async handleOrientationEvents() {
+    if (this.orientationListenerActive) return;
+    this.orientationListenerActive = true;
+    await Motion.addListener('orientation', event => {
+      /* console.log(event); */
+
+      this.alpha = event.alpha
+      this.beta = event.beta
+      this.gamma = event.gamma   
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
